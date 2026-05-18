7a2b73813cb9326836b55593e9d98d71293ec69c
kangcheolung <skhu202114111@gmail.com>
2026-05-18 14:10:38 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/piuda/callcare/CallcareApplication.java
@@ -0,0 +1,13 @@
+package com.piuda.callcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piuda/callcare/CallcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piuda.callcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

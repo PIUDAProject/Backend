58c8f56128020e93cefd319f10b0089c01f696ea
kangcheolung <skhu202114111@gmail.com>
2026-06-24 13:13:55 +0900

    deploy: 재배포

--- src/main/java/com/piuda/callcare/global/config/SecurityConfig.java
+++ src/main/java/com/piuda/callcare/global/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
